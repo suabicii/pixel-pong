8e4af47e12fec05ff7c5a40b2d1cdd9493a2a078
Suabicii <michal.slabik3@wp.pl>
2020-04-27 13:41:13 +0200

    Added movement functions

--- Unit1.cpp
+++ Unit1.cpp
@@ -8,6 +8,16 @@
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
+
+void moveUp(TImage *paddle)
+{
+        if (paddle->Top > 10) paddle->Top -= 10;
+}
+
+void moveDown(TImage *paddle)
+{
+        if (paddle->Top + paddle->Height < Form1->background->Height - 10) paddle->Top += 10;
+}
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
         : TForm(Owner)
